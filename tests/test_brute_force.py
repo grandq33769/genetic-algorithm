04832e19254c72ae352338dd6c200f1590c9bae0
lhleung <grandq33769@gmail.com>
2021-04-06 14:29:26 +0800

    test(brute_force): add time record

--- tests/test_brute_force.py
+++ tests/test_brute_force.py
@@ -1,10 +1,26 @@
+# Standard Library
+from datetime import datetime
+from time import time
+
+# Third Party Library
+import pytest
+
 # Local Module
 from ga.main import brute_force
 
 
+@pytest.mark.slow
 def test_brute_force():
+
+    start = time()
     max_v, min_v = brute_force()
+    end = time()
+
+    with open('./performance/brute_force.csv', 'a') as wf:
+        wf.write(f'{datetime.now().isoformat()},{end-start}')
+
     assert isinstance(max_v, tuple)
     assert isinstance(min_v, tuple)
-    assert max_v == ((1, 1), 3)
+    # max (0.6887, 0.9866)
+    assert max_v == ((0.6887, 0.9866), 2.1912616758946326)
     assert min_v == ((1, 1), 3)
