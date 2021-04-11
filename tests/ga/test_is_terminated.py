810c186693d3af7088f35d1ff0b9f8c64d8cf12f
lhleung <grandq33769@gmail.com>
2021-04-11 10:04:49 +0800

    test(is_terminated): create a "is_terminated_by_generation" test

---
+++ tests/ga/test_is_terminated.py
@@ -0,0 +1,20 @@
+# Local Module
+from ga.main import is_terminated_by_generation
+
+
+def test_is_terminated_by_generation(ga_setting):
+    generation = 2
+    populations = [('010'), ('110')]
+    fitnesses = [(20), (30)]
+    setting = ga_setting
+    input_args = (generation, populations, fitnesses, setting)
+
+    test_success_subject = lambda *args: is_terminated_by_generation(
+        *args, terminated_gen=1
+    )
+    test_fail_subject = lambda *args: is_terminated_by_generation(
+        *args, terminated_gen=3
+    )
+
+    assert test_success_subject(*input_args) == True
+    assert test_fail_subject(*input_args) == False
