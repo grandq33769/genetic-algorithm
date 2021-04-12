0900b1018d615b94bba872b7e25920e80c09124a
lhleung <grandq33769@gmail.com>
2021-04-12 22:50:30 +0800

    feat(mutation): one bit mutation

---
+++ ga/mutation/binary.py
@@ -0,0 +1,27 @@
+# Standard Library
+from operator import attrgetter
+from random import randint
+from typing import List
+
+# Third Party Library
+from loguru import logger as log
+
+# Local Module
+from ga.config import GAsetting
+from ga.utils import separate_pop_by_chance
+
+
+def one_bit_mutation(population: List, setting: GAsetting):
+    mutation_rate, operator = attrgetter('mutation_rate', 'gene_operator')(
+        setting
+    )
+    candidates, new_population = separate_pop_by_chance(
+        population, mutation_rate
+    )
+    occurred: bool = bool(candidates)
+    for candidate in candidates:
+        mutation_idx = randint(0, operator.length - 1)
+        mutated = operator.mutation(candidate, mutation_idx)
+        log.debug(f'{candidate=}, {mutated=}, {mutation_idx=}')
+        new_population.append(mutated)
+    return new_population, occurred
