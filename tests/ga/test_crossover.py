215730e9b5f609a6ae2ae79d613e58979109044f
lhleung <grandq33769@gmail.com>
2021-04-12 22:07:24 +0800

    test(file_logger): add file_logger to test

--- tests/ga/test_crossover.py
+++ tests/ga/test_crossover.py
@@ -3,9 +3,10 @@ from ga.crossover.binary import one_bit_crossover
 from ga.main import basic_init
 
 
-def test_one_bit_crossover(ga_setting):
+def test_one_bit_crossover(ga_setting, file_logger):
     population = list(basic_init(ga_setting)[0])
 
     subject = one_bit_crossover(population, ga_setting)
+    file_logger.debug(subject)
     assert isinstance(subject, list)
     assert len(subject) == ga_setting.pop_size
--- tests/ga/test_evaluation.py
+++ tests/ga/test_evaluation.py
@@ -1,10 +1,14 @@
+# Third Party Library
+from loguru import logger as log
+
 # Local Module
 from ga.main import basic_evaluation, basic_init
 
 
-def test_basic_evaluation(ga_setting):
+def test_basic_evaluation(ga_setting, file_logger):
     population = basic_init(ga_setting)[0]
     subject = basic_evaluation(population, ga_setting)
+    file_logger.debug(subject)
 
     assert isinstance(subject, tuple)
     assert len(subject) == 100
