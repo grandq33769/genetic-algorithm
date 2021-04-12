10212d779fa905c129a1af1fe85d3d9784dc4509
lhleung <grandq33769@gmail.com>
2021-04-12 22:05:32 +0800

    test(file_logger): add file_logger fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,12 @@
 # content of conftest.py
 
+# Standard Library
+import os
+from datetime import datetime
+
 # Third Party Library
 import pytest
+from loguru import logger
 
 
 def pytest_addoption(parser):
@@ -22,3 +27,15 @@ def pytest_collection_modifyitems(config, items):
     for item in items:
         if "slow" in item.keywords:
             item.add_marker(skip_slow)
+
+
+@pytest.fixture
+def file_logger(request):
+    filename = os.path.basename(request.node.fspath).replace('.py', '')
+    now_time = datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
+    handler_id = logger.add(
+        f'logs/{filename}/{now_time}.log',
+        # filter=lambda r: r["extra"]["task"] == filename,
+    )
+    yield logger
+    logger.remove(handler_id)
