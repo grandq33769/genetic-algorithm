87b97ea868fe167d1efc5f7e3fc31fcc9b8e3499
lhleung <grandq33769@gmail.com>
2021-04-13 07:56:28 +0800

    feat(eval/hc): add hc_evaluation method

--- ga/evaluation/basic.py
+++ ga/evaluation/basic.py
@@ -3,11 +3,18 @@ from operator import attrgetter
 from typing import Tuple
 
 # Local Module
-from ga.config import GAsetting
+from ga.config import GAsetting, HillClimbingSetting
 from ga.main import obj_func
 
 
 def basic_evaluation(population: Tuple, setting: GAsetting) -> Tuple:
     operator = attrgetter("gene_operator")(setting)
     fitness = tuple(obj_func(*operator.decode(i)) for i in population)
+    return fitness
+
+
+def hc_evaluation(population: Tuple, setting: HillClimbingSetting) -> Tuple:
+    operator, target = attrgetter("gene_operator", "target")(setting)
+    setting.target_fitness = obj_func(*operator.decode(target))
+    fitness = basic_evaluation(population, setting)
     return fitness
