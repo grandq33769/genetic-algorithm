0434e011a95759131326b2948a3f323e416d2d76
lhleung <grandq33769@gmail.com>
2021-04-13 07:57:25 +0800

    test(eval/hc): add hc_evaluation test

--- tests/ga/test_evaluation.py
+++ tests/ga/test_evaluation.py
@@ -2,7 +2,7 @@
 from loguru import logger as log
 
 # Local Module
-from ga.evaluation.basic import basic_evaluation
+from ga.evaluation.basic import basic_evaluation, hc_evaluation
 from ga.initialization.basic import basic_init
 
 
@@ -14,3 +14,14 @@ def test_basic_evaluation(ga_setting, file_logger):
     assert isinstance(subject, tuple)
     assert len(subject) == 100
     assert all((isinstance(i, float) for i in subject))
+
+
+def test_hc_evaluation(hc_setting):
+    population = basic_init(hc_setting)[0]
+    subject = basic_evaluation(population, hc_setting)
+
+    assert isinstance(subject, tuple)
+    assert len(subject) == 100
+    assert all((isinstance(i, float) for i in subject))
+
+    assert isinstance(hc_setting.target_fitness, float)
