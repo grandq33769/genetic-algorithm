668496702c72ba18dfc25361e07cb677d75eda9a
lhleung <grandq33769@gmail.com>
2021-04-13 03:12:46 +0800

    feat(init): add "init_by_neighbors" method

--- ga/initialization/basic.py
+++ ga/initialization/basic.py
@@ -1,5 +1,6 @@
 # Standard Library
 from operator import attrgetter
+from random import randint
 from typing import List, Tuple
 
 # Local Module
@@ -11,4 +12,18 @@ def basic_init(setting: GAsetting) -> List[Tuple]:
     populations: List[Tuple] = []
     population = tuple(operator.generate() for _ in range(pop_size))
     populations.append(population)
+    return populations
+
+
+def init_by_neighbors(setting: GAsetting) -> List[Tuple]:
+    pop_size, operator = attrgetter("pop_size", "gene_operator")(setting)
+    populations: List[Tuple] = []
+    population: List = []
+    target = operator.generate()
+    while len(population) != pop_size:
+        neighbor_idx = randint(0, operator.length - 1)
+        neighbor = operator.mutation(target, neighbor_idx)
+        if operator.valid_gene(neighbor):
+            population.append(neighbor)
+    populations.append(tuple(population))
     return populations
--- tests/ga/test_init.py
+++ tests/ga/test_init.py
@@ -1,9 +1,20 @@
 # Local Module
-from ga.initialization.basic import basic_init
+from ga.initialization.basic import basic_init, init_by_neighbors
 
 
 def test_basic_init(ga_setting):
     subject = basic_init(ga_setting)
+    assert isinstance(subject, list)
+    assert len(subject) == 1
+
+    population = subject[0]
+    assert isinstance(population, tuple)
+    assert len(population) == 100
+    assert all((isinstance(i, str) for i in population))
+
+
+def test_init_by_neighbors(ga_setting):
+    subject = init_by_neighbors(ga_setting)
     assert isinstance(subject, list)
     assert len(subject) == 1
 
