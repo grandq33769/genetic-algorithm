de28fa2241d514c461b634a5c8b82c593573705c
lhleung <grandq33769@gmail.com>
2021-04-13 11:54:20 +0800

    test(hillclimbing): change the termination function name

--- tests/ga/test_hillclimbing_binary.py
+++ tests/ga/test_hillclimbing_binary.py
@@ -6,7 +6,7 @@ from ga.initialization.basic import init_by_neighbors
 from ga.main import genetic_algorithm
 from ga.mutation.binary import no_ops as mutation_no_ops
 from ga.selection.neighbor import select_neighbor_from_target
-from ga.termination.basic import is_strongest_diff_from_previous
+from ga.termination.basic import smaller_than_previous
 
 from .performance_utils import ga_testing
 
@@ -17,7 +17,7 @@ def test_ga_binary(hc_setting, file_logger):
         init_by_neighbors,
         hc_evaluation,
         select_neighbor_from_target,
-        is_strongest_diff_from_previous,
+        smaller_than_previous,
         crossover_no_ops,
         mutation_no_ops,
     )
