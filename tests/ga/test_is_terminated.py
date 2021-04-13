740dd46c5ce4e52fc8e15f2df5227ce085cc48b8
lhleung <grandq33769@gmail.com>
2021-04-13 08:04:12 +0800

    test(is_terminated/hc): add is_strongest_diff_from_previous test

--- tests/ga/test_is_terminated.py
+++ tests/ga/test_is_terminated.py
@@ -1,5 +1,8 @@
 # Local Module
-from ga.termination.basic import is_terminated_by_generation
+from ga.termination.basic import (
+    is_strongest_diff_from_previous,
+    is_terminated_by_generation,
+)
 
 
 def test_is_terminated_by_generation(ga_setting):
@@ -18,3 +21,19 @@ def test_is_terminated_by_generation(ga_setting):
 
     assert test_success_subject(*input_args) == True
     assert test_fail_subject(*input_args) == False
+
+
+def test_is_strongest_diff_from_previous(hc_setting):
+    generation = 1
+    populations = [('010', '001'), ('110', '100')]
+    fitnesses_success = [(20, 10), (30, 5)]
+    fitnesses_fail = [(10, 10), (8, 5)]
+    hc_setting.target_fitness = 15
+
+    success_args = (generation, populations, fitnesses_success, hc_setting)
+    fail_args = (generation, populations, fitnesses_fail, hc_setting)
+
+    assert is_strongest_diff_from_previous(*success_args) == True
+    assert hc_setting.target_fitness == 30
+    assert is_strongest_diff_from_previous(*fail_args) == False
+    assert hc_setting.target_fitness == 30
