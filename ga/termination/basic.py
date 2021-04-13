d30d158f4bd3956c529c6431288d1ac1016ef8ea
lhleung <grandq33769@gmail.com>
2021-04-13 08:03:12 +0800

    feat(is_terminated/hc): add is_strongest_diff_from_previous()

--- ga/termination/basic.py
+++ ga/termination/basic.py
@@ -5,7 +5,7 @@ from typing import List, Tuple
 from loguru import logger as log
 
 # Local Module
-from ga.config import GAsetting
+from ga.config import GAsetting, HillClimbingSetting
 
 
 def is_terminated_by_generation(
@@ -28,3 +28,41 @@ def is_terminated_by_generation(
         )
     )
     return generation > terminated_gen
+
+
+def is_strongest_diff_from_previous(
+    generation: int,
+    populations: List[Tuple],
+    fitnesses: List[Tuple],
+    setting: HillClimbingSetting,
+):
+    # Can not compare if there is only one generation
+    if generation < 1:
+        return False
+
+    new_pop = populations[-1]
+    new_fit = fitnesses[-1]
+    new_max_value = max(new_fit)
+    new_strongest = new_pop[new_fit.index(new_max_value)]
+
+    operator = setting.gene_operator
+    target = setting.target
+    target_value = setting.target_fitness
+
+    log.debug(
+        ' '.join(
+            [
+                f'{operator=}',
+                f'{new_strongest=}',
+                f'{new_max_value=}',
+                f'{target=}',
+                f'{target_value=}',
+            ]
+        )
+    )
+    replacement: bool = new_max_value > target_value
+    if replacement:
+        setting.target = new_strongest
+        setting.target_fitness = new_max_value
+
+    return replacement
