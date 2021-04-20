4693dbf666428689967580453a62e397d0d5f92f
lhleung <grandq33769@gmail.com>
2021-04-20 23:09:54 +0800

    test(fix/selection): add "obj_func" args for selection

--- tests/ga/algorithm/test_hillclimbing_float_min.py
+++ tests/ga/algorithm/test_hillclimbing_float_min.py
@@ -6,21 +6,27 @@ from ga.initialization.basic import init_by_neighbors
 from ga.main import obj_func
 from ga.mutation.binary import no_ops as mutation_no_ops
 from ga.selection.neighbor import select_neighbor_from_target
-from ga.termination.basic import smaller_than_previous
+from ga.termination.basic import is_terminated_by_generation
 
 from .performance_utils import ga_testing
 
 
 def test_hillclimbing_float_min(hc_setting_float, file_logger):
+    is_terminated = lambda *args: is_terminated_by_generation(
+        *args, terminated_gen=100
+    )
     evaluation = lambda *args: hc_evaluation(
+        *args, obj_func=lambda *x: -obj_func(*x)
+    )
+    selection = lambda *args: select_neighbor_from_target(
         *args, obj_func=lambda *x: -obj_func(*x)
     )
     config = HillClimbingConfig(
         hc_setting_float,
         init_by_neighbors,
         evaluation,
-        select_neighbor_from_target,
-        smaller_than_previous,
+        selection,
+        is_terminated,
         crossover_no_ops,
         mutation_no_ops,
     )
