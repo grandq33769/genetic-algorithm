89bf49578137a7d1b6abb958f81fe0894bc6bb58
lhleung <grandq33769@gmail.com>
2021-04-20 22:57:23 +0800

    fix(selection/neighbor): select the neighbor until local optimal

--- ga/selection/neighbor.py
+++ ga/selection/neighbor.py
@@ -1,17 +1,30 @@
 # Standard Library
 from operator import attrgetter
-from random import randint
-from typing import List, Tuple
+from typing import Callable, List, Tuple
 
 # Third Party Library
 from loguru import logger as log
 
 # Local Module
-from ga.config import HillClimbingSetting
+from ga.config import GAsetting, HillClimbingSetting
+from ga.evaluation.basic import basic_evaluation
+
+
+def get_strongest(
+    population: Tuple, setting: GAsetting, obj_func: Callable
+) -> Tuple[Tuple, int]:
+    new_fit = basic_evaluation(population, setting, obj_func)
+    new_max_value = max(new_fit)
+    new_strongest = population[new_fit.index(new_max_value)]
+
+    return new_strongest, new_max_value
 
 
 def select_neighbor_from_target(
-    population: Tuple, fitness: Tuple, setting: HillClimbingSetting
+    population: Tuple,
+    fitness: Tuple,
+    setting: HillClimbingSetting,
+    obj_func: Callable,
 ) -> List:
     pop_size, operator, target = attrgetter(
         "pop_size", "gene_operator", "target"
@@ -21,14 +34,17 @@ def select_neighbor_from_target(
             [
                 f'{population=}',
                 f'{fitness=}',
-                f'{population=}',
             ]
         )
     )
-    new_population: List = []
-    while len(new_population) != pop_size:
-        neighbor_idx = randint(0, operator.length - 1)
-        neighbor = operator.mutation(target, neighbor_idx)
-        if operator.valid_gene(neighbor):
-            new_population.append(neighbor)
-    return new_population
+    local: bool = False
+    while not local:
+        neighbors = operator.generate_neighbor(target, pop_size)
+        strongest, max_value = get_strongest(neighbors, setting, obj_func)
+
+        if max_value > setting.target_fitness:
+            setting.target, setting.target_fitness = strongest, max_value
+        else:
+            local = True
+
+    return [setting.target]
