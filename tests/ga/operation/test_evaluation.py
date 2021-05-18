51dd86443188ff8e1d32e66ddb4deaadd2ad13c2
lhleung <grandq33769@gmail.com>
2021-05-19 06:48:06 +0800

    test(eval/niching): add niching evaluation test

--- tests/ga/operation/test_evaluation.py
+++ tests/ga/operation/test_evaluation.py
@@ -3,8 +3,10 @@ from loguru import logger as log
 
 # Local Module
 from ga.evaluation.basic import basic_evaluation, hc_evaluation
+from ga.evaluation.niching import niching_evaluation
 from ga.initialization.basic import basic_init, init_by_neighbors
 from ga.main import obj_func
+from ga.objective.hw2 import obj_func_q1
 
 
 def test_basic_evaluation(ga_setting, file_logger):
@@ -25,3 +27,15 @@ def test_hc_evaluation(hc_setting):
     assert len(subject) == hc_setting.pop_size
     assert all((isinstance(i, float) for i in subject))
     assert isinstance(hc_setting.target_fitness, float)
+
+
+def test_niching_evaluation(niching_float_setting, file_logger):
+    population = basic_init(niching_float_setting)[0]
+    subject = niching_evaluation(
+        population, niching_float_setting, obj_func_q1
+    )
+    file_logger.debug(subject)
+
+    assert isinstance(subject, tuple)
+    assert len(subject) == niching_float_setting.pop_size
+    assert all((isinstance(i, float) for i in subject))
